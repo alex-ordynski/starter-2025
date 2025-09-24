51001062f580b46c38da3aba2ce16b0f165666b0
Oleksandr Ordynskyi <Dr.Hikari@gmail.com>
2025-09-24 08:19:57 +0300

    Create setup.sql

---
+++ setup.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS wizards;
+DROP TABLE IF EXISTS houses;
+
+CREATE TABLE houses (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    founder TEXT,
+    animal TEXT
+);
+
+CREATE TABLE wizards (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT,
+    house_id INTEGER,
+    FOREIGN KEY (house_id) REFERENCES houses (id)
+);
+
+INSERT INTO houses (id, name, founder, animal) VALUES
+    (1, 'Gryffindor', 'Godric Gryffindor', 'Lion'),
+    (2, 'Slytherin', 'Salazar Slytherin', 'Serpent'),
+    (3, 'Ravenclaw', 'Rowena Ravenclaw', 'Eagle'),
+    (4, 'Hufflepuff', 'Helga Hufflepuff', 'Badger');
+
+INSERT INTO wizards (first_name, last_name, house_id) VALUES
+    ('Harry', 'Potter', 1),
+    ('Hermione', 'Granger', 1),
+    ('Ron', 'Weasley', 1),
+    ('Draco', 'Malfoy', 2),
+    ('Luna', 'Lovegood', 3);
